ed9734caf0caf5c2eafe798b76c44559b3ef50e2
hiephamk <hiep23000@student.hamk.fi>
2023-09-10 13:19:23 +0300

    Add Functions_And_Method Exercises

---
+++ src/FunctionAndMethod/Method_Exercise_1.java
@@ -0,0 +1,21 @@
+package FunctionAndMethod;
+
+public class Method_Exercise_1 {
+
+	public static void main(String[] args) {
+		printTile();
+		valuesInt(2);
+	}
+	public static void printTile() {
+		System.out.println("********************");
+		System.out.println("* Method Exercises *");
+		System.out.println("********************");
+	}
+	public static void valuesInt(int n) {
+		int coef = 1000;
+		System.out.println(coef*n);
+		
+	}
+	
+
+}
