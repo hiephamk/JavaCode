fa663eeef4bd71e669c80c4493c874c228db6f4f
hiephamk <hiep23000@student.hamk.fi>
2023-09-10 14:27:28 +0300

    Update and add ex_3

--- src/FunctionAndMethod/Method_Exercise_1.java
+++ src/FunctionAndMethod/Method_Exercise_1.java
@@ -1,21 +1,37 @@
 package FunctionAndMethod;
 
+import java.util.Scanner;
+
 public class Method_Exercise_1 {
 
 	public static void main(String[] args) {
 		printTile();
-		valuesInt(2);
+		int number = userNum();	
+		valuesPrint(number);
+
 	}
 	public static void printTile() {
 		System.out.println("********************");
 		System.out.println("* Method Exercises *");
 		System.out.println("********************");
 	}
-	public static void valuesInt(int n) {
-		int coef = 1000;
-		System.out.println(coef*n);
+	public static void valuesPrint(int number) {
+		int e = number*1000;
+		
+		System.out.println(e);
+		
+		
+	}
+	public static int userNum() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Input a number: ");
+		
+		int numIn = sc.nextInt();
+		Scanner close;
+		return numIn;
+
 		
 	}
 	
-
 }
