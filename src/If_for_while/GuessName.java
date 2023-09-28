f9c420fc9b8d3e077b423bc738bed614fca23290
hiephamk <hiep23000@student.hamk.fi>
2023-09-28 12:09:49 +0300

    make a 3 name, and create a name from random 3 names

--- src/If_for_while/GuessName.java
+++ src/If_for_while/GuessName.java
@@ -1,14 +1,20 @@
 package If_for_while;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessName {
 
 	public static void main(String[] args) {
-		
+		Random rd = new Random();
 		Scanner sc = new Scanner(System.in);
-		String name = "Emma";
-		int i = 1;
+		String [] ranName = {"Emma","Julia","John"};
+		//System.out.println(ranName[1]);
+		int b = rd.nextInt(ranName.length);
+		String name = ranName[b];
+		System.out.println(ranName[b]);
+		
+		int i = 0;
 		while(true) {
 			
 			System.out.println("Guess my name (type stop to exit)");
@@ -16,12 +22,13 @@ public class GuessName {
 			String guessName = sc.nextLine();
 			
 			if(guessName.equalsIgnoreCase("stop")) {
-				i-=1;
+				//i-=1;
 				
 				break;
 			}
 			else if(guessName.equals(name)) {
 				System.out.println("Congratulations!");
+				i++;
 				
 				break;
 			}
